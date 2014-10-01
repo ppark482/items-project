fc32e5ca9cf04866a4aa1bc0e4303932b9b1885a
Paul Park <ppark482@gmail.com>
2014-10-01 15:49:47 -0400

    one two three five completed

--- main.js
+++ main.js
@@ -14,6 +14,44 @@ var sum = priceArray.reduce(function(a,b) {
 
 var average = sum / items.length;
 
-console.log('The average price is ' + average.toFixed(2));
+console.log('Q1 : The average price is ' + average.toFixed(2));
 
-// Question 2: 
+// Question 2: Get an array of items that cost between $14.00 and $18.00 USD
+
+var itemTitles = [];
+var arrForRange = items.forEach( function(x){
+  if (x.price > 14 && x.price < 18) {
+    itemTitles.push('title :' + x.title);
+  };
+});
+
+console.log(itemTitles);
+
+// Question 3: Find the item with a "GBP" currency code and print its name and price.
+var currencyCode = [];
+var currencyGBP = items.filter( function(y) {
+  if (y.currency_code === "GBP") {
+    currencyCode.push(y.title + " costs \u00A3" + y.price);
+  }
+})
+
+console.log(currencyCode);
+
+// Question 4: Find which items are made of wood.
+var madeWithWood = [];
+var woodItem = items.forEach( function(z) {
+  if (z.materials == "wood") {
+    madeWithWood.push(z.description);
+  }
+});
+
+console.log(madeWithWood);
+
+// Question 5: Find which items are made of eight or more materials.
+var moreThanEight = [];
+var lotsOfMats = items.filter( function(m) {
+  var mats = m.materials;
+  if (mats.length >= 8) {
+    console.log(m.title, m.materials);
+  }
+});
